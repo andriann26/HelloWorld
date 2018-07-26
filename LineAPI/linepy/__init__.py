a26e185c57f49eb12f939649ac0b1d0abe58f2f5
andriann26 <41375379+andriann26@users.noreply.github.com>
2018-07-26 15:10:59 +0000

    Update __init__.py

--- LineAPI/linepy/__init__.py
+++ LineAPI/linepy/__init__.py
@@ -3,7 +3,7 @@ from .channel import Channel
 from .oepoll import OEPoll
 from ..akad.ttypes import OpType
 
-__copyright__       = 'Copyright 2018 by Fadhiil Rachman'
+__copyright__       = 'Copyright 2018 by Andrian'
 __version__         = '3.0.8'
 __license__         = 'BSD-3-Clause'
 __author__          = 'Fadhiil Rachman'
