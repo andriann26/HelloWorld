9d41425aa561bc5aebaf7d01913db3cf587d97f9
andriann26 <41375379+andriann26@users.noreply.github.com>
2018-07-26 15:12:19 +0000

    Update __init__.py

--- LineAPI/linepy/__init__.py
+++ LineAPI/linepy/__init__.py
@@ -6,8 +6,8 @@ from ..akad.ttypes import OpType
 __copyright__       = 'Copyright 2018 by Andrian'
 __version__         = '3.0.8'
 __license__         = 'BSD-3-Clause'
-__author__          = 'Fadhiil Rachman'
-__author_email__    = 'fadhiilrachman@gmail.com'
+__author__          = 'Andriann'
+__author_email__    = 'unknowm@gmail.com'
 __url__             = 'https://github.com/andriann26/helloworld'
 
 __all__ = ['LINE', 'Channel', 'OEPoll', 'OpType']
